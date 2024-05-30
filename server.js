e39e758c5cb0e04005a717d8786e65e2c5df1eb1
Caio <caiobarreto1acim@gmail.com>
2024-05-30 01:09:40 -0300

    Primeiro registro API

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const app = express()
+app.use(express.json())
+
+const user = []
+
+app.post('/usuarios', (req,res) =>{
+    user.push(req.body)
+    res.status(201).json(req.body)
+})
+
+
+app.get('/usuarios', (req, res) =>{
+    res.status(200).json(user)
+})
+
+app.listen(3000)
